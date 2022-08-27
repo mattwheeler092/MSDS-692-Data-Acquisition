cda047c3b533ff7349407b1e74464f50a6c9dd45
Matt Wheeler <mtwheeler@usfca.edu>
2022-08-26 23:33:23 -0700

    script to convert json data to csv format

---
+++ assigment_1/json2csv.py
@@ -0,0 +1,21 @@
+import json
+
+from mycsv import getdata
+
+# Helper functions to create CSV string
+add_line = lambda line: ",".join(line) + "\n"
+
+# Load JSON file passed as terminal argument
+data = json.loads(getdata())
+
+# Extract headers and data from JSON dictionary
+headers = data["headers"]
+data = [list(record.values()) for record in data["data"]]
+
+# Create csv output string
+csv = add_line(headers)
+for line in data:
+    csv += add_line(line)
+
+# Return the string to the terminal
+print(csv)
