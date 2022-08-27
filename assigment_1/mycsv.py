2d366ab53e5dadf476b761f954e538ed65bf891d
Matt Wheeler <mtwheeler@usfca.edu>
2022-08-26 18:18:17 -0700

    Added functionality to read and process csv file passed through terminal arguments

--- assigment_1/mycsv.py
+++ assigment_1/mycsv.py
@@ -1,7 +1,8 @@
 import sys
 
+
 def getdata():
-    if len(sys.argv)==1: # if no file given, read from stdin
+    if len(sys.argv) == 1:
         data = sys.stdin.read()
     else:
         f = open(sys.argv[1], "r")
@@ -9,11 +10,12 @@ def getdata():
         f.close()
     return data.strip()
 
+
 def readcsv(data):
     """
     Read CSV with header from data string and return a header list
     containing a list of names and also return the list of lists
     containing the data.
     """
-    ...
+    headers, *data = [line.split(",") for line in data.split("\n")]
     return headers, data
