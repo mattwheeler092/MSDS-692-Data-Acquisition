8c844cf568b878c0c972fcbcf88b4c52d0ef9e98
Matt Wheeler <mtwheeler@usfca.edu>
2022-09-14 16:46:36 -0700

    File now print the top ten most common word in the xml file provided via terminal

--- assignment_3/common.py
+++ assignment_3/common.py
@@ -1,6 +1,11 @@
-from tfidf import *
 import sys
 
-xmltext = ...
+from tfidf import *
 
-...
+with open(sys.argv[1], "r") as file:
+    xmltext = file.read()
+text = gettext(xmltext)
+word_count = Counter(tokenizer(text))
+most_common = sorted(word_count, key=lambda x: word_count[x], reverse=True)
+for key in most_common[:10]:
+    print(key, word_count[key])
